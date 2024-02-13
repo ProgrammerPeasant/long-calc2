4ed2fbc87974117360ee3ed23be2cb9b613b0440
Богдан <63032909+ProgrammerPeasant@users.noreply.github.com>
2024-02-13 11:24:08 +0300

    Delete cmake-build-debug-coverage directory

--- cmake-build-debug-coverage/Google_tests/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/antonbirka/Documents/long-calc2/long-calc2/Google_tests/lib/googletest
-# Build directory: /Users/antonbirka/Documents/long-calc2/long-calc2/cmake-build-debug-coverage/Google_tests/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
