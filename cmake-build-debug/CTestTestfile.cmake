6a2f103443ead5d7c83d5af2175045c15e292cde
invoker(1000) <63032909+anus23@users.noreply.github.com>
2024-02-13 22:26:40 +0300

    New tests

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/admin/CLionProjects/long-calc2
+# Build directory: C:/Users/admin/CLionProjects/long-calc2/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Google_tests")
