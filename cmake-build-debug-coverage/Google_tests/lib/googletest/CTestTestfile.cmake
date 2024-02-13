15a5000d39b04af445f99e8eba8964353f446756
TrapsAreFuture <poland-fish@yandex.ru>
2024-02-13 11:22:23 +0300

    New version

---
+++ cmake-build-debug-coverage/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/antonbirka/Documents/long-calc2/long-calc2/Google_tests/lib/googletest
+# Build directory: /Users/antonbirka/Documents/long-calc2/long-calc2/cmake-build-debug-coverage/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ cmake-build-debug/CMakeFiles/LongCalcLib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LongCalcLib.dir/lib/InfFloat.cpp.o"
+  "CMakeFiles/LongCalcLib.dir/lib/InfFloat.cpp.o.d"
+  "libLongCalcLib.a"
+  "libLongCalcLib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LongCalcLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
