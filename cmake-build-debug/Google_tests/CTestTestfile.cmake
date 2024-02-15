cf8d7a880ebc934557233aafca8883c356509bc0
Богдан <63032909+ProgrammerPeasant@users.noreply.github.com>
2024-02-15 23:58:01 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/Google_tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/admin/CLionProjects/long-calc2/Google_tests
-# Build directory: C:/Users/admin/CLionProjects/long-calc2/cmake-build-debug/Google_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
